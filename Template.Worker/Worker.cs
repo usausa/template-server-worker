e947aa19b0e97692636988953d18091d376c8b91
usausa <machi.pon@gmail.com>
2021-11-06 21:32:45 +0900

    Add project

---
+++ Template.Worker/Worker.cs
@@ -0,0 +1,28 @@
+namespace Template.Worker
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public class Worker : BackgroundService
+    {
+        private ILogger<Worker> Log { get; }
+
+        public Worker(ILogger<Worker> log)
+        {
+            Log = log;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Log.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}

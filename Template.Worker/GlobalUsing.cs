080f7d007f4a351b060164d52efe006ebc69e13a
usausa <machi.pon@gmail.com>
2022-02-10 16:55:46 +0900

    Update global using

--- Template.Worker/GlobalUsing.cs
+++ Template.Worker/GlobalUsing.cs
@@ -6,7 +6,12 @@ global using System.ComponentModel.DataAnnotations;
 global using System.Data.Common;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
+global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
 
 global using Microsoft.Extensions.Logging;
+
+global using Smart;
+global using Smart.Collections.Generic;
+global using Smart.Text;
